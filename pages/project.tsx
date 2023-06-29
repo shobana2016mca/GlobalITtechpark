71c6475447ee23cb44caf1298ebdbfe1b1842aee
shobana <ursweetshobana.r@gmail.com>
2023-06-30 00:22:05 +0530

    jun29 last commit

--- pages/project.tsx
+++ pages/project.tsx
@@ -133,7 +133,7 @@ const AboutUs = () => {
               src="/images/GTP_Project_7.jpg"
               alt="..."
             />
-            <p className="pt-4 text-left">BOSCH Bengaluru</p>
+            <p className="pt-4 text-left">BOSCH Bangalor</p>
           </div>
         </div>
         <div className="flex-auto px-4 md:w-1/4">
